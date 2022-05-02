7cb0bcd145a2f7d7bff3a59861993a7501a77eea
rajpawanku <pawangrk00@gmail.com>
2022-05-02 16:09:24 +0530

    push

--- components/navbar.js
+++ components/navbar.js
@@ -1,1 +1,5 @@
-let url =""
+function navbar(){
+    return`<a href="index.html"><p>News App</p></a> 
+    <input type="text" id="search_input" placeholder="Search News" onclick="search()">`
+}
+export{navbar}
